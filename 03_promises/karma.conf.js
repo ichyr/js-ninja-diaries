d6a73ac582be7e744932ab5b49f49b7740e14f34
Ivan Chyr <ichyr@softserveinc.com>
2015-12-13 08:19:49 +0100

    first failing test

--- 03_promises/karma.conf.js
+++ 03_promises/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '"js/*.js", "test/*.spec.js"'
+      'js/*.js', 'test/*.spec.js'
     ],
 
 
---
+++ 03_promises/test/deferred.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Deferred object', function() {
+	it('should be defined', function() {
+		expect(new Deferred()).toBeDefined();
+	});
+});
