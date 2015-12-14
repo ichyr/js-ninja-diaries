7eb4dead2544ef079da013466be3fbaea87b11e5
Ivan Chyr <ichyr@softserveinc.com>
2015-12-14 09:04:02 +0100

    added use case with undefined setter

--- 04_read_only_property.js
+++ 04_read_only_property.js
@@ -1,45 +1,46 @@
 // Ways of creating read-only property
 
 // 1. Object.defineProperty
-(function (argument) {
+(function() {
 	'use strict';
-function readOnlyObject1() {
 
-	// set up and define read-only promise instance variable
-	var val = 'readOnlyValue';
-	Object.defineProperty(this, 'prop', {
-		writable: false,
-		value: val
-	});
-};
+	function readOnlyObject1() {
+		var val = 'readOnlyValue';
+		Object.defineProperty(this, 'prop', {
+			writable: false,
+			value: val
+		});
+	};
 
-var test = new readOnlyObject1();
-console.log(test.prop);
-test.prop = 'changed';
-console.log(test.prop);
+	var test = new readOnlyObject1();
+	console.log(test.prop);
+	test.prop = 'changed';
+	console.log(test.prop);
 })();
 
 // => Uncaught TypeError: Cannot assign to read only property 'prop' of #<readOnlyObject1>
 
 
-// 2.
+// 2. Using getter and setter definition inside Object.defineProperty
 
-(function (argument) {
+(function() {
 	'use strict';
-function readOnlyObject2() {
-	// set up and define read-only promise instance variable
-	var val = 'readOnlyValue';
-};
 
-readOnlyObject2.generateProperty('prop', {
-  get: function() {
-    return this.val;
-  },
-  set: false
-});
+	function readOnlyObject2() {
+		var val = 'readOnlyValue';
+		Object.defineProperty(this, 'prop', {
+			get: function() {
+				return this.val;
+			},
+			set: undefined
+		});
+	};
 
-var test = new readOnlyObject2();
-console.log(test.prop);
-test.prop = 'changed';
-console.log(test.prop);
+
+	var test = new readOnlyObject2();
+	console.log(test.prop);
+	test.prop = 'changed';
+	console.log(test.prop);
 })();
+
+// => Uncaught TypeError: Cannot set property prop of #<readOnlyObject2> which has only a getter
